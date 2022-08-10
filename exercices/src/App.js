02597406c2e27aa5480ac3f1a7327b4300df16e0
Richárd Nagy <nagy.richard@thisisdone.com>
2022-08-10 15:08:27 +0200

    Update exercices 1.3.-1.5.

--- exercices/src/App.js
+++ exercices/src/App.js
@@ -21,40 +21,42 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 };
 
 const App = () => {
-    const course = "Half Stack application development";
-    const part1 = "Fundamentals of React";
-    const exercises1 = 10;
-    const part2 = "Using props to pass data";
-    const exercises2 = 7;
-    const part3 = "State of a component";
-    const exercises3 = 14;
+    const course = {
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+            },
+        ],
+    };
 
     return (
         <div>
-            <Header course={course} />
+            <Header course={course.name} />
             <Content
-                part={[part1, part2, part3]}
-                exercises={[exercises1, exercises2, exercises3]}
+                part={[course.parts[0].name, course.parts[1].name, course.parts[2].name]}
+                exercises={[
+                    course.parts[0].exercises,
+                    course.parts[1].exercises,
+                    course.parts[2].exercises,
+                ]}
             />
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Total
+                exercises1={course.parts[0].exercises}
+                exercises2={course.parts[1].exercises}
+                exercises3={course.parts[2].exercises}
+            />
         </div>
     );
-
-    // return (
-    //     <div>
-    //         <h1>{course}</h1>
-    //         <p>
-    //             {part1} {exercises1}
-    //         </p>
-    //         <p>
-    //             {part2} {exercises2}
-    //         </p>
-    //         <p>
-    //             {part3} {exercises3}
-    //         </p>
-    //         <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    //     </div>
-    // );
 };
 
 export default App;
