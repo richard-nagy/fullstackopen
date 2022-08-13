3cdb05b924077adbe217c3fe79d76481ece3c478
Richárd Nagy <nagy.richard@thisisdone.com>
2022-08-13 22:28:41 +0200

    Add part2 e;

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,12 +2,34 @@ import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from "./services/notes";
 
+const Notification = ({ message }) => {
+    if (message === null) {
+        return null;
+    }
+
+    return <div className="error">{message}</div>;
+};
+
+const Footer = () => {
+    const footerStyle = {
+        color: "green",
+        fontStyle: "italic",
+        fontSize: 16,
+    };
+    return (
+        <div style={footerStyle}>
+            <br />
+            <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+        </div>
+    );
+};
+
 const App = ({ propsNotes }) => {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState("");
     const [showAll, setShowAll] = useState(true);
+    const [errorMessage, setErrorMessage] = useState("some error happened...");
 
-    // npx json-server --port 3001 --watch db.json
     useEffect(() => {
         noteService.getAll().then((initialNotes) => {
             setNotes(initialNotes);
@@ -44,6 +66,10 @@ const App = ({ propsNotes }) => {
             })
             .catch(() => {
                 alert(`the note '${note.content}' was already deleted from server`);
+                setErrorMessage(`Note '${note.content}' was already removed from server`);
+                setTimeout(() => {
+                    setErrorMessage(null);
+                }, 5000);
                 setNotes(notes.filter((n) => n.id !== id));
             });
     };
@@ -52,8 +78,8 @@ const App = ({ propsNotes }) => {
 
     return (
         <div>
-            <h1>b; Forms</h1>
-            <h2>Notes</h2>
+            <h1>Notes</h1>
+            <Notification message={errorMessage} />
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? "important" : "all"}
@@ -72,6 +98,7 @@ const App = ({ propsNotes }) => {
                 <input value={newNote} onChange={handleNoteChange} />
                 <button type="submit">save</button>
             </form>
+            <Footer />
         </div>
     );
 };
