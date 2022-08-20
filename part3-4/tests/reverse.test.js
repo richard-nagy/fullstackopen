ed4f8b172266151d1009115207f687505c255ebb
Richárd Nagy <nagy.richard@thisisdone.com>
2022-08-20 23:57:32 +0200

    Add part3-4 without its git parts

---
+++ part3-4/tests/reverse.test.js
@@ -0,0 +1,51 @@
+const reverse = require("../utils/for_testing").reverse;
+
+const average = (array) => {
+    const reducer = (sum, item) => {
+        return sum + item;
+    };
+
+    return array.length === 0 ? 0 : array.reduce(reducer, 0) / array.length;
+};
+
+describe("average", () => {
+    test("of one value is the value itself", () => {
+        expect(average([1])).toBe(1);
+    });
+
+    test("of many is calculated right", () => {
+        expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5);
+    });
+
+    test("of empty array is zero", () => {
+        expect(average([])).toBe(0);
+    });
+});
+
+test("reverse of a", () => {
+    const result = reverse("a");
+
+    expect(result).toBe("a");
+});
+
+test("reverse of react", () => {
+    const result = reverse("react");
+
+    expect(result).toBe("tcaer");
+});
+
+test("reverse of releveler", () => {
+    const result = reverse("releveler");
+
+    expect(result).toBe("releveler");
+});
+
+// test("palindrom of react", () => {
+//     const result = reverse("react");
+
+//     expect(result).toBe("tkaer");
+// });
+
+test("of empty array is zero", () => {
+    expect(average([])).toBe(0);
+});
