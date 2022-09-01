402699f1515b5de3e2eb4cab3e2b0e128152cfe3
Richárd Nagy <nagy.richard@thisisdone.com>
2022-09-01 20:20:22 +0200

    Add Exercises 6.1.-6.8.

---
+++ exercices/redux-anecdotes-main/src/components/AnecdoteForm.js
@@ -0,0 +1,28 @@
+import { useDispatch } from "react-redux";
+import { newAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch();
+
+    const addNew = (event) => {
+        event.preventDefault();
+        dispatch(newAnecdote(event.target.content.value));
+        event.target.content.value = "";
+    };
+
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+
+            <h2>create new</h2>
+            <form onSubmit={addNew}>
+                <div>
+                    <input name="content" />
+                </div>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    );
+};
+
+export default AnecdoteForm;
